f7dce712968e4000a3d8616fab9ce8e72c86de41
Anees <skanees007@gmail.com>
2023-05-17 07:20:30 +0200

    Second Session

--- FirstProject/src/FirstPackage/FirstProgram.java
+++ FirstProject/src/FirstPackage/FirstProgram.java
@@ -4,13 +4,6 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		
-		System.out.println("AutomationTesting Training");
-		System.out.println("Hello All");
-	
-		System.out.println("Hello My Automation Team member");
-		
-		System.out.println("Hi Soujanya");
-	}
 
+}
 }
