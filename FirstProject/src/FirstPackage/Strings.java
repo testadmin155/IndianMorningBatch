3bc7cd05146fa88b62accbe791cfe8b6b65c1de3
Anees <skanees007@gmail.com>
2023-05-18 07:31:15 +0200

    Operator and Arrays

--- FirstProject/src/FirstPackage/Strings.java
+++ FirstProject/src/FirstPackage/Strings.java
@@ -6,12 +6,26 @@ public class Strings {
 		
 		String x = "Testing";
 		String text = "Welcome to Automation Testing Training";
-		System.out.println(text.length());
+		/*System.out.println(text.length());
 		System.out.println(text.toUpperCase());
 		System.out.println(text.toLowerCase());
 		
 		boolean s = text.equalsIgnoreCase("welcome to Automation testing Training");
 		System.out.println(s);
+		*/
+		//if condition
+		String expectedmessage = "Payment is succesful and your order is placed";
+		String actualmessage = "Payment is succefu and your order is placed";
+		
+		if(expectedmessage.equals(actualmessage))
+		{
+			System.out.println("Test case is passed");
+		}
+		else
+		
+			{
+				System.out.println("Test case is Failed");
+			}
 		
 		
 
