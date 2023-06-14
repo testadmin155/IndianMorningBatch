0e022b3fadf0e3a79ead760fa926458f7f26104a
Anees <skanees007@gmail.com>
2023-06-14 17:57:48 +0200

    Ruchika practise GIT session

---
+++ SeleniumProject/src/AutomationPackage/PRactiseClass.java
@@ -0,0 +1,11 @@
+package AutomationPackage;
+
+public class PRactiseClass {
+
+	public static void main(String[] args) {
+		System.out.println("Bye Ruchika");
+		System.out.println("Have a nice Evening");
+
+	}
+
+}
